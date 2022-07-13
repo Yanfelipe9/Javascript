cc247f70f3aa4ff1def97980b6352e6119c17bde
Yan Felipe <yanfelipe9@hotmail.com>
2022-07-12 22:58:38 -0300

    ex010

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro foi de ${vel} Km/hr`)
+
+if (vel>=60){
+    console.log(`Voçe ultrapaçou a velocidade permitida, voçe foi MULTADO, com uma velocidade de ${vel}`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Voçe é Brasileiro')
+} else {
+    console.log('Voçe é Estrangeiro')
+}

61a0278a0f34f8612052afa2cda1c6b924cb79ce
Yan Felipe <yanfelipe9@hotmail.com>
2022-07-14 12:22:27 -0300

    implementando cores

--- aula012ex/ex015/scrips.js
+++ aula012ex/ex015/scrips.js
@@ -18,28 +18,38 @@ function verificar() {
         if(fsexo[0].checked){
             genero = 'Homem'
             if(idade >= 0 && idade < 5){
+                document.body.style.background = '#574350'
                 img.src = 'imagens/homem-bebe.png'
             } else if(idade <= 12){
+                document.body.style.background = '#947B7E'
                 img.src = 'imagens/homem-crianca.png'
             } else if(idade <= 30){
+                document.body.style.background ='#ABAEB2'
                 img.src = 'imagens/homem-jovem.png'
             } else if(idade <= 55){
+                document.body.style.background = '#545E81'
                 img.src = 'imagens/homem-adulto.png'
             } else if(idade <= 150){
+                document.body.style.background = '#C1BDB7'
                 img.src = 'imagens/homem-idoso.png'
             } 
            
         } else if(fsexo[1].checked){
             genero = 'Mulher'
             if(idade >= 0 && idade < 5){
+                document.body.style.background = '#B4C875'
                 img.src = 'imagens/mulher-bebe.png'
             }else if(idade <= 12){
+                document.body.style.background = '#4A4934'
                 img.src = 'imagens/mulher-crianca.png'
             } else if(idade <= 30){
+                document.body.style.background = '#77524B'
                 img.src = 'imagens/mulher-jovem.png'
             } else if(idade <= 55){
+                document.body.style.background = '#E9B57F'
                 img.src = 'imagens/mulher-adulta.png'
             } else if(idade <= 150){
+                document.body.style.background = '#BFADA5'
                 img.src = 'imagens/mulher-idosa.png'
             } 
         } 
@@ -48,6 +58,11 @@ function verificar() {
     }
 
     res.innerHTML = `${genero} com ${idade} anos`
+
+
+}
+
+
 
 /*
     var img = document.querySelector('#imagem')
@@ -78,7 +93,7 @@ function verificar() {
 */
         
 
-}
+
 
 
 

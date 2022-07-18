86fd0e040893017efcf8e231a5b2c646aa6c8bd8
Yan Felipe <yanfelipe9@hotmail.com>
2022-07-18 16:28:32 -0300

    ex1

--- aula014ex/ex017/scrips.js
+++ aula014ex/ex017/scrips.js
@@ -1,19 +1,24 @@
 
 function contar(){
-    var inicioTxt = document.querySelector('#inicio')
-    inicio = Number(inicioTxt.value)
-    
-    var fimTxt = document.querySelector('#fim')
-    fim = Number(fimTxt.value)
+    let inicio = document.querySelector('#iniciotxt')
+    let fim = document.querySelector('#fimtxt')
+    let passo = document.querySelector('#passotxt')
+    let res = document.querySelector('#res')
 
-    var fimTxt = document.querySelector('#fim')
-    fim = Number(fimTxt.value)
-
-    var res = document.querySelector('#res')
-
-    for(inicio ; inicio <= fim ; inicio++){
-        res.innerHTML = inicio
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERROR] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        for(let c = i; i<f ; c += p){
+            res.innerHTML += `${c}`
+        }
     }
+
+    
 
 }
 
